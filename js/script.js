2c8078c9d70887a4f1702a18325acdb13a3b39e2
Anum <127afarghani@frhsd.com>
2025-11-17 12:52:19 -0500

    vars

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var dropDown = 
+
+var boxName = document.getElementById("nameInput");
+console.log(boxName);
+
+var boxAge = document.getElementById("ageInput");
+console.log(boxAge);
+
+var favColor = document.getElementById("colorInput");
+console.log(favColor);
+
+var btnCreate = document.getElementById("submitBtn");
+console.log(btnCreate);
+
+var btnProfile = document.getElementById("output");
+console.log(btnProfile);
+
