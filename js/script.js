cdaef06d99f2146a8f0ef8515c7fc5e64eb0727d
Anum <127afarghani@frhsd.com>
2025-11-18 13:56:19 -0500

    innerhtml fixed

--- js/script.js
+++ js/script.js
@@ -15,12 +15,19 @@ console.log(btnCreate);
 var btnProfile = document.getElementById("output");
 console.log(btnProfile);
 
-var favPet = document.querySelectorAll("input[type='radio'")
+var favPet = document.querySelectorAll("input")
 console.log(favPet);
+
+var hobBy = document.getElementById("hobby");
 
 btnCreate.addEventListener('click', myFunction);
 
 function myFunction (){
-    const nameUser = nameInput.value;
-    btnProfile.innerHTML = 'Name', nameUser + colorInput
+ const nameUser = nameInput.value;
+ const ageUser = ageInput.value;
+ const petUser = favPet.value;
+ const hobbyUser = hobBy.value;
+ const colorUser = colorInput.options[colorInput.selectedIndex].text;
+ btnProfile.innerHTML = ('Name:') + (' ') + (nameUser) + ('Age:') + (' ') +  (ageUser) + ('Favorite Color:') + (' ') +  (colorUser) + ('Pet:') + (' ') +  (petUser) + ('Hobbies:') + (' ') +  (hobbyUser);
+
 }
